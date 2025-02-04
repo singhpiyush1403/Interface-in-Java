5c18a32d231e4f542fc8219b178d4e22ea0cc003
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-02-04 19:41:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+interface Animal {
+    void eat(); 
+    void sleep();
+}
+
+
+class Dog implements Animal {
+    public void eat() {
+        System.out.println("Dog is eating.");
+    }
+
+    public void sleep() {
+        System.out.println("Dog is sleeping.");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.eat();
+        myDog.sleep();
+    }
+}
+
+
